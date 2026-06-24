f070aab0b365dc2587110b25e567620e99907afd
ws-personax <wsphp2024@gmail.com>
2026-06-24 23:26:27 +0900

    refactor: split persona prompts into layered modules (DNA/rules/fewshots/safety/output-style)

---
+++ lib/personax/personas/echo/index.ts
@@ -0,0 +1,5 @@
+export { ECHO_DNA } from './dna';
+export { ECHO_RULES } from './rules';
+export { ECHO_FEW_SHOTS } from './fewshots';
+export { ECHO_SAFETY } from './safety';
+export { ECHO_OUTPUT_STYLE } from './output-style';
